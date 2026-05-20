80630b9da61c7452517ed20acb4d0cda4ffbb385
Idhibhat Pankam <marginpankam@gmail.com>
2026-05-20 13:55:29 +0700

    1

--- 3-modularity/user.cpp
+++ 3-modularity/user.cpp
@@ -1,11 +1,29 @@
 #include "vector.h"
 #include <cmath>
+#include <iostream>
+#include <stdexcept>
 
 using namespace std;
+
+void f(Vector &v) {
+    try {
+        v[v.size()] = 7; // try to access beyond the end of v
+    } catch (std::out_of_range &e) {
+        std::cerr << "Error: " << e.what() << '\n';
+    }
+}
 
 double sqrt_sum(Vector &v) {
     double sum = 0;
     for (int i = 0; i != v.size(); ++i)
         sum += sqrt(v[i]); // sum of square roots
     return sum;
+}
+
+int main(void) {
+    Vector v(3);
+    v[2] = 2;
+    cout << sqrt_sum(v) << '\n';
+    f(v);
+    return 0;
 }
--- 3-modularity/vector.cpp
+++ 3-modularity/vector.cpp
@@ -1,9 +1,14 @@
 #include "vector.h"
+#include <stdexcept>
 
 Vector::Vector(int s)
     : elem{new double[s]}, sz{s} // initialize members
 {}
 
-double &Vector::operator[](int i) { return elem[i]; }
+double &Vector::operator[](int i) {
+    if (i < 0 || size() <= i)
+        throw std::out_of_range{"Vector::operator[]"};
+    return elem[i];
+}
 
 int Vector::size() { return sz; }
