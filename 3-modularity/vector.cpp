d2ddd6edabe8eeda60edf392fb7781d81cd81470
Idhibhat Pankam <marginpankam@gmail.com>
2026-05-20 13:52:13 +0700

    1

---
+++ 3-modularity/vector.cpp
@@ -0,0 +1,9 @@
+#include "vector.h"
+
+Vector::Vector(int s)
+    : elem{new double[s]}, sz{s} // initialize members
+{}
+
+double &Vector::operator[](int i) { return elem[i]; }
+
+int Vector::size() { return sz; }
