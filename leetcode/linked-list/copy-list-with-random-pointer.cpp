8e8d02603edb3edcf1c63426b7c809409df4dc03
Idhibhat Pankam <marginpankam@gmail.com>
2026-06-28 16:10:28 +0700

    1

--- leetcode/linked-list/copy-list-with-random-pointer.cpp
+++ leetcode/linked-list/copy-list-with-random-pointer.cpp
@@ -14,48 +14,50 @@ public:
 };
 */
 
-#include <unordered_map>
-
 struct Node {
     int val;
     Node *next;
     Node *random;
     Node(int _val) : val(_val), next(nullptr), random(nullptr) {}
 };
+
 class Solution {
   public:
     Node *copyRandomList(Node *head) {
-        std::unordered_map<Node *, Node *> node2copy;
+        if (!head)
+            return nullptr;
+
+        // 1. copy
         Node *curr = head;
-        Node *dummy = new Node(0);
-        Node *curr2 = dummy;
-
         while (curr != nullptr) {
-            if (node2copy.find(curr) == node2copy.end()) {
-                Node *newNode = new Node(curr->val);
-                node2copy[curr] = newNode;
-                curr2->next = newNode;
-            } else {
-                curr2->next = node2copy[curr];
-            }
-            // cout << "curr " << curr->val << "\n";
-
-            curr2 = curr2->next;
-
-            if (curr->random == nullptr) {
-            } else if (node2copy.find(curr->random) == node2copy.end()) {
-                Node *newNode = new Node(curr->random->val);
-                node2copy[curr->random] = newNode;
-                curr2->random = newNode;
-            } else {
-                curr2->random = node2copy[curr->random];
-            }
-
-            curr = curr->next;
+            Node *newNode = new Node(curr->val);
+            newNode->next = curr->next;
+            curr->next = newNode;
+            curr = newNode->next;
+            ;
         }
 
-        Node *tmp = dummy->next;
-        delete dummy;
-        return tmp;
+        // 2. random
+        curr = head;
+        while (curr != nullptr) {
+            if (curr->random != nullptr) {
+                curr->next->random = curr->random->next;
+            }
+            curr = curr->next->next;
+        }
+
+        // 3. unweave
+        curr = head;
+        Node *newHead = curr->next;
+        Node *newCurr = newHead;
+        while (curr != nullptr) {
+            curr->next = curr->next->next;
+            newCurr->next = (newCurr->next != nullptr) ? newCurr->next->next : nullptr;
+
+            curr = curr->next;
+            newCurr = newCurr->next;
+        }
+
+        return newHead;
     }
 };
