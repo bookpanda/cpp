bbad9c9ceaa9cc8fa22c1f2358358ff490b1a2e5
Idhibhat Pankam <marginpankam@gmail.com>
2026-06-28 11:17:15 +0700

    w

--- leetcode/linked-list/reorder-list.cpp
+++ leetcode/linked-list/reorder-list.cpp
@@ -1,3 +1,4 @@
+#include <vector>
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -8,34 +9,43 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
- class Solution {
-    public:
-        void reorderList(ListNode* head) {
-            std::vector<ListNode*> nodes;
-            ListNode* curr = head;
-            while (curr != nullptr) {
-                nodes.push_back(curr);
-                curr = curr->next;
-            }
-    
-            int left = 0;
-            int right = nodes.size() - 1;
-            while (left < right) {
-                nodes[left]->next = nodes[right];
-                left++;
-                if (left >= right) break;
-                nodes[right]->next = nodes[left];
-                right--;
-            }
-            // CRITICAL: Terminate the list to prevent cycles and memory corruption
-            nodes[left]->next = nullptr;
-    
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+  public:
+    void reorderList(ListNode *head) {
+        std::vector<ListNode *> nodes;
+        ListNode *curr = head;
+        while (curr != nullptr) {
+            nodes.push_back(curr);
+            ListNode *tmp = curr->next;
+            curr->next = nullptr;
+            curr = tmp;
         }
-    };
-    // 1->2->3->4->5
-    // [1,2,3,4,5]
-    //  l       r
-    //    l   r
-    
-    // 1->2
-    // l. r
+
+        int left = 0;
+        int right = nodes.size() - 1;
+        while (left < right) {
+            nodes[left]->next = nodes[right];
+            left++;
+            if (left >= right)
+                break;
+            nodes[right]->next = nodes[left];
+            right--;
+        }
+    }
+};
+// 1->2->3->4->5
+// [1,2,3,4,5]
+//  l       r
+//    l   r
+
+// 1->2
+// l. r
