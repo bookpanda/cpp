5bfda8277c0828c2d0cd97ebea5bc00c6049aab9
Idhibhat Pankam <marginpankam@gmail.com>
2026-06-28 11:09:07 +0700

    1

---
+++ leetcode/linked-list/reorder-list.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+ class Solution {
+    public:
+        void reorderList(ListNode* head) {
+            std::vector<ListNode*> nodes;
+            ListNode* curr = head;
+            while (curr != nullptr) {
+                nodes.push_back(curr);
+                curr = curr->next;
+            }
+    
+            int left = 0;
+            int right = nodes.size() - 1;
+            while (left < right) {
+                nodes[left]->next = nodes[right];
+                left++;
+                if (left >= right) break;
+                nodes[right]->next = nodes[left];
+                right--;
+            }
+            // CRITICAL: Terminate the list to prevent cycles and memory corruption
+            nodes[left]->next = nullptr;
+    
+        }
+    };
+    // 1->2->3->4->5
+    // [1,2,3,4,5]
+    //  l       r
+    //    l   r
+    
+    // 1->2
+    // l. r
