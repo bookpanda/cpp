fc2b59deceb4a938735dc5d6aa916acff62f0aea
Idhibhat Pankam <marginpankam@gmail.com>
2026-07-14 16:41:18 +0700

    1

---
+++ cpp-concurrency/4-waiting-for-condition/14-spawn_task.cpp
@@ -0,0 +1,13 @@
+#include <future>
+#include <thread>
+#include <type_traits>
+#include <utility>
+
+template <typename F, typename A> std::future<typename std::result_of<F(A &&)>::type> spawn_task(F &&f, A &&a) {
+    typedef typename std::result_of<F(A &&)>::type result_type;
+    std::packaged_task<result_type(A &&)> task(std::move(f));
+    std::future<result_type> res(task.get_future());
+    std::thread t(std::move(task), std::move(a));
+    t.detach();
+    return res;
+}
