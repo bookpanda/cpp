3494e5f6daa848b68e44731e8526f48a640a9895
Idhibhat Pankam <marginpankam@gmail.com>
2026-07-15 18:03:24 +0700

    1

--- cpp-concurrency/5-atomic-types/12-order-relaxed-w-fence.cpp
+++ cpp-concurrency/5-atomic-types/12-order-relaxed-w-fence.cpp
@@ -12,9 +12,10 @@ void write_x_then_y() {
 }
 
 void read_y_then_x() {
-    // load_y is guaranteed to see result of store_y since it loops until it sees true
+    // load_y is guaranteed to see result of store_y since it loops until it sees true (store_y -> load_y)
     // load_y (acq) sees result of a store after a release fence, so release fence -> load_y (syncs-with)
     // load_y is before acquire fence and sees result of store_y (release), so store_y -> acquire fence (syncs-with)
+    // total:  release fence -> store_y -> load_y -> acquire fence -> load_x
     while (!y.load(std::memory_order_relaxed))
         ;
     std::atomic_thread_fence(std::memory_order_acquire);
