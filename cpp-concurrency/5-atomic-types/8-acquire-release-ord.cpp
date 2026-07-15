61fc16102ba029dad99839d4ffe48428edcedfa1
Idhibhat Pankam <marginpankam@gmail.com>
2026-07-15 16:04:21 +0700

    1

--- cpp-concurrency/5-atomic-types/8-acquire-release-ord.cpp
+++ cpp-concurrency/5-atomic-types/8-acquire-release-ord.cpp
@@ -5,15 +5,21 @@
 std::atomic<bool> x, y;
 std::atomic<int> z;
 void write_x_then_y() {
+    // since same thread: store_x -> store_y
     x.store(true, std::memory_order_relaxed);
     y.store(true, std::memory_order_release);
 }
 void read_y_then_x() {
+    // since same thread && load_y is looped until passes: load_y -> load_x
     while (!y.load(std::memory_order_acquire))
         ;
     if (x.load(std::memory_order_relaxed))
         ++z;
 }
+
+// sync-with: store_y sync-with load_y
+
+// summary: store_x -> store_y sync-with load_y -> load_x
 
 int main() {
     x = false;
