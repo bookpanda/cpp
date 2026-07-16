c8868b65072abe0a8ff46d47eb53416e65a275f8
Idhibhat Pankam <marginpankam@gmail.com>
2026-07-16 15:14:15 +0700

    1

--- cpp-concurrency/6-lock-based-data-structures/7-queue-from-list-more.cpp
+++ cpp-concurrency/6-lock-based-data-structures/7-queue-from-list-more.cpp
@@ -17,25 +17,30 @@ template <typename T> class threadsafe_queue {
         return tail;
     }
     std::unique_ptr<node> pop_head() {
-        // head_mutex must be acquired before tail_mutex
-        // if not, can be cases where 2 threads call pop_head(), both acquires tail_mutex, but 1 of them stuck between
-        // get_tail() and head_mutex. And when that stuck thread can acquire head_mutex, it's tail might no longer point
-        // to tail node = comparison head.get() == get_tail() fails even if head is last node
-        std::lock_guard<std::mutex> head_lock(head_mutex);
-        if (head.get() == get_tail()) {
-            return nullptr;
-        }
-        // data isn't modified until the locks are acquired, so it is exception-safe (only mutex locks can throw in this
-        // case)
         std::unique_ptr<node> old_head = std::move(head);
         head = std::move(old_head->next);
         return old_head;
+    }
+    std::unique_lock<std::mutex> wait_for_data() {
+        std::unique_lock<std::mutex> head_lock(head_mutex);
+        data_cond.wait(head_lock, [&] { return head.get() != get_tail(); });
+        return std::move(head_lock);
+    }
+    std::unique_ptr<node> wait_pop_head() {
+        std::unique_lock<std::mutex> head_lock(wait_for_data());
+        return pop_head();
+    }
+    std::unique_ptr<node> wait_pop_head(T &value) {
+        std::unique_lock<std::mutex> head_lock(wait_for_data());
+        value = std::move(*head->data);
+        return pop_head();
     }
 
   public:
     threadsafe_queue() : head(new node), tail(head.get()) {}
     threadsafe_queue(const threadsafe_queue &other) = delete;
     threadsafe_queue &operator=(const threadsafe_queue &other) = delete;
+
     std::shared_ptr<T> try_pop() {
         // pop_head() locks both head_lock and tail_lock
         // if a try_pop() on T1 is concurrent with a push() on T2, 2 cases:
@@ -44,6 +49,13 @@ template <typename T> class threadsafe_queue {
         std::unique_ptr<node> old_head = pop_head();
         return old_head ? old_head->data : std::shared_ptr<T>();
     }
+
+    std::shared_ptr<T> wait_and_pop() {
+        std::unique_ptr<node> const old_head = wait_pop_head();
+        return old_head->data;
+    }
+    void wait_and_pop(T &value) { std::unique_ptr<node> const old_head = wait_pop_head(value); }
+
     void push(T new_value) {
         std::shared_ptr<T> new_data(std::make_shared<T>(std::move(new_value)));
         std::unique_ptr<node> p(new node);
