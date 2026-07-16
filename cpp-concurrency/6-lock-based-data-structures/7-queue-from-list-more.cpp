5ca89d23410d3017f6acaeee82c5cb817ea6629b
Idhibhat Pankam <marginpankam@gmail.com>
2026-07-16 14:50:44 +0700

    1

---
+++ cpp-concurrency/6-lock-based-data-structures/7-queue-from-list-more.cpp
@@ -0,0 +1,59 @@
+#include <memory>
+#include <mutex>
+
+template <typename T> class threadsafe_queue {
+  private:
+    struct node {
+        std::shared_ptr<T> data;
+        std::unique_ptr<node> next;
+    };
+    std::mutex head_mutex;
+    std::unique_ptr<node> head;
+    std::mutex tail_mutex;
+    node *tail;
+    node *get_tail() {
+        std::lock_guard<std::mutex> tail_lock(tail_mutex);
+        return tail;
+    }
+    std::unique_ptr<node> pop_head() {
+        // head_mutex must be acquired before tail_mutex
+        // if not, can be cases where 2 threads call pop_head(), both acquires tail_mutex, but 1 of them stuck between
+        // get_tail() and head_mutex. And when that stuck thread can acquire head_mutex, it's tail might no longer point
+        // to tail node = comparison head.get() == get_tail() fails even if head is last node
+        std::lock_guard<std::mutex> head_lock(head_mutex);
+        if (head.get() == get_tail()) {
+            return nullptr;
+        }
+        // data isn't modified until the locks are acquired, so it is exception-safe (only mutex locks can throw in this
+        // case)
+        std::unique_ptr<node> old_head = std::move(head);
+        head = std::move(old_head->next);
+        return old_head;
+    }
+
+  public:
+    threadsafe_queue() : head(new node), tail(head.get()) {}
+    threadsafe_queue(const threadsafe_queue &other) = delete;
+    threadsafe_queue &operator=(const threadsafe_queue &other) = delete;
+    std::shared_ptr<T> try_pop() {
+        // pop_head() locks both head_lock and tail_lock
+        // if a try_pop() on T1 is concurrent with a push() on T2, 2 cases:
+        // 1. get_tail() before push(): tail points to old dummy node
+        // 2. get_tail() after push(): tail points to new dummy node
+        std::unique_ptr<node> old_head = pop_head();
+        return old_head ? old_head->data : std::shared_ptr<T>();
+    }
+    void push(T new_value) {
+        // allocate mem for new_value and p can throw, but both use smart ptrs, so they'll be freed if exception is
+        // throw
+        std::shared_ptr<T> new_data(std::make_shared<T>(std::move(new_value)));
+        std::unique_ptr<node> p(new node);
+        node *const new_tail = p.get();
+        // blocks until tail_mutex is free, then acquires it.
+        std::lock_guard<std::mutex> tail_lock(tail_mutex);
+        // once lock acquired, none of remaining ops can throw, so push() is exception-safe
+        tail->data = new_data;
+        tail->next = std::move(p);
+        tail = new_tail;
+    }
+};
