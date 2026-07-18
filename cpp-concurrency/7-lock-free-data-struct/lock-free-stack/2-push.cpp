4dd4e48518ba3b21541b73705e8a6ea18fbc1edc
Idhibhat Pankam <marginpankam@gmail.com>
2026-07-18 10:55:43 +0700

    1

---
+++ cpp-concurrency/7-lock-free-data-struct/lock-free-stack/2-push.cpp
@@ -0,0 +1,21 @@
+#include <atomic>
+
+template <typename T> class lock_free_stack {
+  private:
+    struct node {
+        T data;
+        node *next;
+        node(T const &data_) : data(data_) {}
+    };
+    std::atomic<node *> head;
+
+  public:
+    void push(T const &data) {
+        node *const new_node = new node(data);
+        // new node points to head
+        new_node->next = head.load();
+        // head is now new node
+        while (!head.compare_exchange_weak(new_node->next, new_node))
+            ;
+    }
+};
