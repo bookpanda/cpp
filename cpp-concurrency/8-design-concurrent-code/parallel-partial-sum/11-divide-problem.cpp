701c09b6df4a491d1c90ea62f5ee56cb33bae9ba
Idhibhat Pankam <marginpankam@gmail.com>
2026-07-20 15:23:28 +0700

    1

--- cpp-concurrency/8-design-concurrent-code/parallel-partial-sum/11-divide-problem.cpp
+++ cpp-concurrency/8-design-concurrent-code/parallel-partial-sum/11-divide-problem.cpp
@@ -28,17 +28,21 @@ template <typename Iterator> void parallel_partial_sum(Iterator first, Iterator 
                 ++end;
                 std::partial_sum(begin, end, begin);
                 if (previous_end_value) {
+                    // if you're not the first chunk, wait for previous_end_value from previous chunk
                     value_type &addend = previous_end_value->get();
                     *last += addend;
                     if (end_value) {
+                        // update last value first so that other threads can see the updated value early
                         end_value->set_value(*last);
                     }
+                    // then update remaining values in the chunk
                     std::for_each(begin, last, [addend](value_type &item) { item += addend; });
                 } else if (end_value) {
                     end_value->set_value(*last);
                 }
             } catch (...) {
                 if (end_value) {
+                    // will propagate to next chunks when .get() is called
                     end_value->set_exception(std::current_exception());
                 } else {
                     throw;
@@ -49,6 +53,7 @@ template <typename Iterator> void parallel_partial_sum(Iterator first, Iterator 
     unsigned long const length = std::distance(first, last);
     if (!length)
         return;
+
     unsigned long const min_per_thread = 25;
     unsigned long const max_threads = (length + min_per_thread - 1) / min_per_thread;
     unsigned long const hardware_threads = std::thread::hardware_concurrency();
@@ -61,6 +66,7 @@ template <typename Iterator> void parallel_partial_sum(Iterator first, Iterator 
     previous_end_values.reserve(num_threads - 1);
     join_threads joiner(threads);
     Iterator block_start = first;
+
     for (unsigned long i = 0; i < (num_threads - 1); ++i) {
         Iterator block_last = block_start;
         std::advance(block_last, block_size - 1);
@@ -70,6 +76,7 @@ template <typename Iterator> void parallel_partial_sum(Iterator first, Iterator 
         ++block_start;
         previous_end_values.push_back(end_values[i].get_future());
     }
+
     Iterator final_element = block_start;
     std::advance(final_element, std::distance(block_start, last) - 1);
     process_chunk()(block_start, final_element, (num_threads > 1) ? &previous_end_values.back() : 0, 0);
