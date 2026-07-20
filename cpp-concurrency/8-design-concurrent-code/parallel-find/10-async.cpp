83fbd38b9305ee74ef2647dce6e9bcc96b103b85
Idhibhat Pankam <marginpankam@gmail.com>
2026-07-20 14:39:13 +0700

    1

--- cpp-concurrency/8-design-concurrent-code/parallel-find/10-async.cpp
+++ cpp-concurrency/8-design-concurrent-code/parallel-find/10-async.cpp
@@ -18,10 +18,18 @@ Iterator parallel_find_impl(Iterator first, Iterator last, MatchType match, std:
             return last;
         } else {
             Iterator const mid_point = first + (length / 2);
+            // [midpoint, last] is processed by async
             std::future<Iterator> async_result =
                 std::async(&parallel_find_impl<Iterator, MatchType>, mid_point, last, match, std::ref(done));
+            // [first, midpoint] is processed by this thread
             Iterator const direct_result = parallel_find_impl(first, mid_point, match, done);
+            // if [first, midpoint] returns last, which is mid_point (no match), then wait for async result
             return (direct_result == mid_point) ? async_result.get() : direct_result;
+            // 2 cases for async:
+            // 1. async call is deferred, it will run then .get() is called, will be skipped if match found in
+            // direct_result
+            // 2. async call is running on separate thread, destructor will wait for it to complete, so no leaking
+            // threads
         }
     } catch (...) {
         done = true;
