59325a6cd15296b0ad2190ced8afca376db8843d
Idhibhat Pankam <marginpankam@gmail.com>
2026-08-28 10:40:42 +0200

    1

---
+++ leetcode/1d-dp/longest-palindromic-substring.cpp
@@ -0,0 +1,42 @@
+#include <string>
+
+class Solution {
+  private:
+    bool isPalindrome(const std::string &s, int st, int ed) {
+        while (st < ed) {
+            if (s[st++] != s[ed--])
+                return false;
+        }
+        return true;
+    }
+
+  public:
+    std::string longestPalindrome(std::string s) {
+        // writing expand around center time O(n^2), space O(1)
+        // you could write Manacher's algo: time O(n), space O(n), but overkill
+        int start = 0;
+        int maxLen = 0;
+
+        // [&] = captures all local vars in surrounding scope by ref (since need to modify start, maxLen)
+        auto expand = [&](int left, int right) {
+            while (left >= 0 && right < s.size() && s[left] == s[right]) {
+                left--;
+                right++;
+            }
+            int len = right - left - 1;
+            if (maxLen < len) {
+                maxLen = len;
+                start = left + 1;
+            }
+        };
+
+        for (int i = 0; i < s.size(); i++) {
+            expand(i, i);     // odd length
+            expand(i, i + 1); // even length
+        }
+
+        return s.substr(start, maxLen);
+    }
+};
+
+// naive: for start and finish of substring , then check if palin (O(n^2))
